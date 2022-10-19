e0114a379ad6d656f23984ebbb173986a2a6c17d
NMH <nm.huong6497@gmail.com>
2022-10-19 20:20:19 +0700

    Table Layout

---
+++ app/src/main/java/com/huong/appchonhinh/ListImageActivity2.java
@@ -0,0 +1,14 @@
+package com.huong.appchonhinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListImageActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_image2);
+    }
+}
---
+++ app/src/main/java/com/huong/appchonhinh/MainActivity.java
@@ -0,0 +1,57 @@
+package com.huong.appchonhinh;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] arrAnimals;
+    ImageView imgRandom, imgPick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgRandom = findViewById(R.id.imgRandom);
+        imgPick = findViewById(R.id.imgPick);
+
+        randomImage();
+
+        Collections.shuffle(Arrays.asList(arrAnimals));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_refresh:
+                randomImage();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void randomImage() {
+        Random random = new Random();
+        arrAnimals = getResources().getStringArray(R.array.array_animals);
+        int indexRandom = random.nextInt(arrAnimals.length);
+        int resourceImageRandom = getResources().getIdentifier(arrAnimals[indexRandom], "drawable", getPackageName());
+        imgRandom.setImageResource(resourceImageRandom);
+    }
+}
